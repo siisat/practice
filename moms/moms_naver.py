8851048c5b74694262d660c87486a8efe0f0e64e
siisat <mybluething@naver.com>
2026-01-08 11:06:18 +0900

    naver 완성

--- moms/moms_naver.py
+++ moms/moms_naver.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 ############
 # 정기배송, 생일상 등 특이한 주문 상품번호 필요함-필터링용
@@ -110,3 +111,117 @@ gr_naver['판매처'] = '스마트스토어'
 # 파일 추출
 gr_naver = gr_naver.drop('옵션정보', axis=1)
 gr_naver.to_csv('naver_output.csv', index=False, encoding='utf-8-sig')
+
+
+
+
+
+
+
+
+item_list = ['탕국', '나물5종', '명태살전',	'동그랑땡',	'부추전', 
+             '북어포', '깐밤', '대추', '침조기(중)', '침조기(대)', '민어', '돔', '삼색전', '애호박전',
+             '깻잎전', '두부전', '육전', '새우튀김', '오징어튀김', '쥐포튀김', '고구마튀김', '소고기산적',
+             '상어산적', '홍합산적', '갈비찜', '잡채', '소불고기', '기타']
+# '기타' 항목은 매장 주문 건 메모를 위한 항목, 온라인 주문에서는 접수되지 않을 것을 전제로 함.
+
+
+# 옵션명 동일한 표기로 전처리
+# 주문서에 없어서 표기 모르는 것들은 ## 표시
+correction_dict = {
+    '탕국 1kg': '탕국',
+    '나물5종(500g)': '나물5종',
+    '명태살전350g': '명태살전',
+    '동그랑땡(1팩에14개)': '동그랑땡',
+    '부추전3장': '부추전',
+    '북어포1미': '북어포',
+    '깐밤100g': '깐밤',
+    '건대추130g': '대추',
+    '침조기(중)': '침조기(중)',
+    '침조기(대)': '침조기(대)',
+    '민어': '민어', ##
+    '돔': '돔',
+    '삼색전4장': '삼색전',
+    '애호박전': '애호박전', ##
+    '깻잎전380g': '깻잎전',
+    '두부전(1모)': '두부전',
+    '육전250g': '육전',
+    '새우튀김280g': '새우튀김',
+    '오징어튀김230g': '오징어튀김',
+    '쥐포튀김230g': '쥐포튀김',
+    '고구마튀김250g': '고구마튀김',
+    '소고기산적': '소고기산적', ##
+    '상어산적': '상어산적', ##
+    '홍합산적': '홍합산적', ##
+    '궁중갈비찜900g': '갈비찜',
+    '궁중잡채500g': '잡채',
+    '소불고기볶음 350g(+11000원)': '소불고기',
+    '소불고기볶음350g': '소불고기',
+    '통문어자숙': '통문어자숙', ##
+    '빨간고기': '빨간고기',
+    '생김치': '생김치', ##
+    '나박김치': '나박김치', ##
+    '수제식혜900ml': '식혜',
+    '민어조기(중)': '민어조기(중)', ##
+    '민어조기(소) 3미': '민어조기(소) 3미', ##
+    '두부계란전': '두부계란전', ##
+    '사과(특)1개(+8000원)': '사과',
+    '배(특)1개(+10000원)': '배',
+}
+
+
+def standardize_items(item_list):
+    # 항목이 correction_dict에 있다면 바꾸고, 없으면 strip만 해서 두기
+    return [correction_dict.get(item.strip(), item.strip()) for item in item_list]
+
+
+# 표준화 : 리스트 내부 요소별로 correction_dict 적용
+gr_naver['옵션정리버전'] = gr_naver['옵션정리버전'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+gr_naver['옵션정리버전'] = gr_naver['옵션정리버전'].apply(standardize_items)
+
+
+# {항목 : 수량} 딕셔너리 생성
+def expand_items_quantities(row):
+    items = ast.literal_eval(row['옵션정리버전']) if isinstance(row['옵션정리버전'], str) else row['옵션정리버전']
+    quantities = ast.literal_eval(row['수량']) if isinstance(row['수량'], str) else row['수량']
+
+    return dict(zip(items, quantities))
+
+
+
+
+# 옵션 수량 체크
+def count_items(df, item_list):
+    buyers = df['주문번호'].tolist()
+    counts = []
+    for row in df.itertuples(index=False):
+        # {항목 : 수량} 딕셔너리 생성
+        d = expand_items_quantities({'옵션정리버전': getattr(row, '옵션정리버전'), '수량': getattr(row, '수량')})
+        # 지정한 item_list 순서대로 수량 리스트 생성, 없는 항목은 0
+        counts.append([d.get(item, 0) for item in item_list])
+    result = pd.DataFrame(counts, columns=item_list, index=buyers)
+    result.index.name = '주문번호'
+    return result
+
+counted_naver = count_items(gr_naver, item_list)
+counted_naver.to_csv('naver_counted.csv', encoding='utf-8-sig')
+
+
+# 수량 체크한 버전에 배송 정보 포함한 최종본 엑셀 생성
+# gr_naver + counted_naver 데이터프레임 병합 = fin_naver
+fin_naver = pd.merge(gr_naver, counted_naver, on='주문번호', how='outer')
+fin_naver2 = fin_naver.drop(['주문번호', '상품번호', '수량', '옵션정리버전', '배송희망일자_현관비번'], axis=1)
+# fin_naver2['주문자'] = fin_naver2['수령인'] # 자사몰은 주문자=수령인
+
+# 열 순서 변경
+# 구매자명,수취인명,배송희망일자_현관비번,배송일,공동현관비번,판매처
+
+all_columns = fin_naver2.columns.tolist()
+all_columns.remove('구매자명')
+move_cols = ['배송일', '공동현관비번', '판매처']
+for col in move_cols:
+    all_columns.remove(col)
+new_order = ['구매자명'] + all_columns + move_cols
+fin_naver2 = fin_naver2[new_order]
+
+fin_naver2.to_csv('naver_final.csv', encoding='utf-8-sig')
